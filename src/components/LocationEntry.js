f394997a0090089abc09e5d92d503f13a4009c70
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-07-27 14:31:05 -0700

    added api sdk package for census

--- src/components/LocationEntry.js
+++ src/components/LocationEntry.js
@@ -1,2 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+import census from 'citysdk';
+import dotenv from 'dotenv'
+dotenv.config()
+
+const apiAccessKey = process.env.CENSUS_TOKEN
+
+// population sdk call
+census({
+    vintage: '2021',
+    geoHierarchy: {
+        state:'04',
+        'metropolitan statistical area/micropolitan statistical area': '*',
+    },
+    sourcePath: ['acs', 'acs1','profile'],
+    values: ['BB01001_001E'], // GINI index
+    statsKey: apiAccessKey,
+    geoResolution: '500k',
+},   
+(err, res) => console.log(res)
+)
+// median income sdk calls
+census({
+    vintage: '2021',
+    geoHierarchy: {
+        state:'04',
+        'metropolitan statistical area/micropolitan statistical area': '*',
+    },
+    sourcePath: ['acs', 'acs1','profile'],
+    values: ['B17001_002E'], // GINI index
+    statsKey: apiAccessKey,
+    geoResolution: '500k',
+},   
+(err, res) => console.log(res)
+)
