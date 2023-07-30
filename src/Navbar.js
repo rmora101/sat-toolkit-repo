276c5459ed411b78ed3b143a00c6231763facbd0
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-07-29 18:39:04 -0700

    added title to page

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>SAT Toolkit</h1>
+            <div className="links">
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
