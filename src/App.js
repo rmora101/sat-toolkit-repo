6d82a58697ad2e4ab102ade600257068f0e3ebbc
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-07-29 20:04:17 -0700

    added input area to page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Navbar from './containers/Navbar';
 import { useState } from 'react';
 import './App.css';
 import BasicTable from './containers/Table.tsx';
+import Input from './containers/Cityselect.tsx';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <div className='content'>
+      <section className='table'>
       <BasicTable></BasicTable>
-      </div>
+      </section>
+      <section className='cityInput'>
+        <Input></Input>
+      </section>
     </div>
   );
 }
