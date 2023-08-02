69f72effe7911f2dc52ab530dbb6a3932f3387f5
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-08-02 14:17:00 -0700

    added state dropdown menu

--- src/containers/Cityselect.tsx
+++ src/containers/Cityselect.tsx
@@ -1,63 +1,51 @@
 import * as React from 'react';
-// import InputLabel from '@mui/material/InputLabel';
-// import MenuItem from '@mui/material/MenuItem';
-// import FormHelperText from '@mui/material/FormHelperText';
-// import FormControl from '@mui/material/FormControl';
-// import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-// export default function SelectLabels() {
-//   const [age, setAge] = React.useState('');
+export default function SelectLabels() {
+  const [states, setState] = React.useState('');
 
-//   const handleChange = (event: SelectChangeEvent) => {
-//     setAge(event.target.value);
-//   };
+  const handleChange = (event: SelectChangeEvent) => {
+    setState(event.target.value);
+  };
 
-//   return (
-//     <div>
-//       <FormControl sx={{ m: 1, minWidth: 120 }}>
-//         <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
-//         <Select
-//           labelId="demo-simple-select-helper-label"
-//           id="demo-simple-select-helper"
-//           value={age}
-//           label="Age"
-//           onChange={handleChange}
-//         >
-//           <MenuItem value="">
-//             <em>None</em>
-//           </MenuItem>
-//           <MenuItem value={10}>Ten</MenuItem>
-//           <MenuItem value={20}>Twenty</MenuItem>
-//           <MenuItem value={30}>Thirty</MenuItem>
-//         </Select>
-//         <FormHelperText>With label + helper text</FormHelperText>
-//       </FormControl>
-//       <FormControl sx={{ m: 1, minWidth: 120 }}>
-//         <Select
-//           value={age}
-//           onChange={handleChange}
-//           displayEmpty
-//           inputProps={{ 'aria-label': 'Without label' }}
-//         >
-//           <MenuItem value="">
-//             <em>None</em>
-//           </MenuItem>
-//           <MenuItem value={10}>Ten</MenuItem>
-//           <MenuItem value={20}>Twenty</MenuItem>
-//           <MenuItem value={30}>Thirty</MenuItem>
-//         </Select>
-//         <FormHelperText>Without label</FormHelperText>
-//       </FormControl>
-//     </div>
-//   );
-// }
-
-const Input = () => {
-    return (
-        <nav className="navbar">
-            <h1>Input</h1>
-        </nav>
-    );
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">State</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={states}
+          label="states"
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={0o4}>Arizona</MenuItem>
+        </Select>
+      </FormControl>
+      {/* <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <Select
+          value={states}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label' }}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+        <FormHelperText>Without label</FormHelperText>
+      </FormControl> */}
+    </div>
+  );
 }
 
-export default Input;
