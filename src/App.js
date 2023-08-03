177910d7be500f1c25d873781876b1aac6381830
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-08-02 22:30:58 -0700

    added figma html file and css for default build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './containers/Navbar';
-import { useState } from 'react';
 import './App.css';
 import BasicTable from './containers/Table.tsx';
 import Input from './containers/Cityselect.tsx';
