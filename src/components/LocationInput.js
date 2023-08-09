ab002bb9afca17733e4042e2aa59b9442d5c6508
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-08-09 13:19:18 -0700

    unworking branch

--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -1,26 +1,29 @@
 import axios from "axios";
 import {selectedStateId} from "../containers/Stateselect.tsx";
 
-const URL = "https://api.census.gov/data/2021/acs/acs1?get=NAME,B01001_001E&for=public%20use%20microdata%20area:*&in=state:04"
+// const URL = "https://api.census.gov/data/2021/acs/acs1?get=NAME,B01001_001E&for=public%20use%20microdata%20area:*&in=state:04"
 const authToken = process.env.CENSUS_TOKEN;
 
 
 export const getStates = () => {
 
-    const response = axios.get("https://api.census.gov/data/2016/acs/acs1?get=NAME&for=state:*")
+    const response = axios.get(`https://api.census.gov/data/2016/acs/acs1?get=NAME&for=state:*&key=${authToken}`)
         .then((response) => {
             const states = []
             for (let data of response.data) {
                 const name = data[0];   // Accessing the "NAME" field
                 const state = data[1];  // Accessing the "state" field
                 const stateData = {'stateName': name, 'stateId': state};
-                states.push(stateData)
+                states.push(stateData);
             }
-            return states
+            return states;
         })
-        .catch(() => {return'hello'});
-        return response
-}
+        .catch((error) => {
+            console.error("Error fetching states:", error);
+            return [];
+    });
+    return response;
+};
 
 // create a conditional where the second dropdown does not work until state id variable has a number.
 
@@ -41,7 +44,7 @@ export const getStates = () => {
 // }
 
 export const getIncomeRace = () => {
-    const response = axios.get(`https://api.census.gov/data/2021/acs/acs1/profile?get=NAME,DP03_0062E,DP05_0078PE,DP05_0071PE,DP05_0037PE,DP05_0044PE&for=public%20use%20microdata%20area:00119&in=state:04`)
+    const response = axios.get(`https://api.census.gov/data/2021/acs/acs1/profile?get=NAME,DP03_0062E,DP05_0078PE,DP05_0071PE,DP05_0037PE,DP05_0044PE&for=public%20use%20microdata%20area:00119&in=state:04&key=${authToken}`)
     .then((response) => {
         const stats = []
         for (let data of response.data) {
@@ -61,7 +64,7 @@ export const getIncomeRace = () => {
 }
 
 export const getPopulation = () => {
-    const response = axios.get(`https://api.census.gov/data/2021/acs/acs1?get=NAME,B01001_001E&for=public%20use%20microdata%20area:00119&in=state:04`)
+    const response = axios.get(`https://api.census.gov/data/2021/acs/acs1?get=NAME,B01001_001E&for=public%20use%20microdata%20area:00119&in=state:04&key=${authToken}`)
     .then((response) => {
         const population = []
         for (let data of response.data) {
