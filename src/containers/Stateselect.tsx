3280bc531596753a27b1d2efa5bc696bdb67533f
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-08-09 21:55:20 -0700

    adjusted dropdown size

--- src/containers/Stateselect.tsx
+++ src/containers/Stateselect.tsx
@@ -8,65 +8,8 @@ import {getStates} from '../components/LocationInput.js';
 import {useEffect} from 'react';
 
 
-
-// export default function SelectLabels() {
-//   const [states, setStates] = React.useState({});
-
-//   const handleChange = (event: SelectChangeEvent) => {
-//     const selectedStateId = event.target.value;
-//     setStates(selectedStateId);
-//   };
-
-//   useEffect(() => { 
-//     const fetchStates = async () => {
-//       const defaultStates = await getStates();
-//       setStates(defaultStates);
-//     };
-//     fetchStates();
-
-//   }, []);
-// export default function SelectLabels() {
-//   const [selectedStateId, setSelectedStateId] = React.useState(''); // Initialize with an empty string
-
-//   const handleChange = (event: SelectChangeEvent) => {
-//     setSelectedStateId(event.target.value); // Update selectedStateId instead of states
-//   };
-
-//   useEffect(() => {
-//     const fetchStates = async () => {
-//       const defaultStates = await getStates();
-//       selectedStateId(defaultStates);
-//     };
-//     fetchStates();
-//   }, []);
-
-//   return (
-//     <div>
-//       <FormControl sx={{ m: 1, minWidth: 120 }}>
-//         <InputLabel id="demo-simple-select-helper-label">State</InputLabel>
-//         <Select
-//           labelId="demo-simple-select-helper-label"
-//           id="demo-simple-select-helper"
-//           value={selectedStateId}
-//           label="states"
-//           onChange={handleChange}
-//         >
-//           <MenuItem value="">
-//             <em>None</em>
-//           </MenuItem>
-//           {Object.entries(selectedStateId).map(([k,v]) => (
-//         <MenuItem key ={k} value={v.stateId}></MenuItem>
-//       ))}
-//         </Select>
-//         <FormHelperText>Required</FormHelperText>
-//       </FormControl>
-
-//     </div>
-//   );
-// }
-
 export let selectedStateId = '';
-console.log(selectedStateId)
+  console.log(selectedStateId)
 
 export default function SelectLabels() {
   const [selectedStateId, setSelectedStateId] = React.useState('');
@@ -89,7 +32,7 @@ export default function SelectLabels() {
 
   return (
     <div>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
+      <FormControl sx={{ m: 2, minWidth: 120, minHeight:190 }}>
         <InputLabel id="demo-simple-select-helper-label">State</InputLabel>
         <Select
           labelId="demo-simple-select-helper-label"
@@ -97,17 +40,19 @@ export default function SelectLabels() {
           value={selectedStateId}
           label="states"
           onChange={handleChange}
+          sx={{"& .MuiSelect-select":{paddingTop: 5,
+            paddingBottom: 0.5,}}}
         >
-          <MenuItem value="">
+          {/* <MenuItem value="">
             <em>None</em>
-          </MenuItem>
+          </MenuItem> */}
           {states.map((state) => (
             <MenuItem key={state['stateId']} value={state['stateId']}>
               {state['stateName']}
             </MenuItem>
           ))} 
         </Select>
-        <p>{selectedStateId}</p>
+        {/* <p>{selectedStateId}</p> */}
         <FormHelperText>Required</FormHelperText>
       </FormControl>
     </div>
