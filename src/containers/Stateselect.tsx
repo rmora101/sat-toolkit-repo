38fc16bfdec0bfb4d0a89be2f77662a032890884
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-08-09 22:17:08 -0700

    refactored tables

--- src/containers/Stateselect.tsx
+++ src/containers/Stateselect.tsx
@@ -6,65 +6,7 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import {getStates} from '../components/LocationInput.js';
 import {useEffect} from 'react';
-import { sizing } from '@mui/system';
 
-
-
-// export default function SelectLabels() {
-//   const [states, setStates] = React.useState({});
-
-//   const handleChange = (event: SelectChangeEvent) => {
-//     const selectedStateId = event.target.value;
-//     setStates(selectedStateId);
-//   };
-
-//   useEffect(() => { 
-//     const fetchStates = async () => {
-//       const defaultStates = await getStates();
-//       setStates(defaultStates);
-//     };
-//     fetchStates();
-
-//   }, []);
-// export default function SelectLabels() {
-//   const [selectedStateId, setSelectedStateId] = React.useState(''); // Initialize with an empty string
-
-//   const handleChange = (event: SelectChangeEvent) => {
-//     setSelectedStateId(event.target.value); // Update selectedStateId instead of states
-//   };
-
-//   useEffect(() => {
-//     const fetchStates = async () => {
-//       const defaultStates = await getStates();
-//       selectedStateId(defaultStates);
-//     };
-//     fetchStates();
-//   }, []);
-
-//   return (
-//     <div>
-//       <FormControl sx={{ m: 1, minWidth: 120 }}>
-//         <InputLabel id="demo-simple-select-helper-label">State</InputLabel>
-//         <Select
-//           labelId="demo-simple-select-helper-label"
-//           id="demo-simple-select-helper"
-//           value={selectedStateId}
-//           label="states"
-//           onChange={handleChange}
-//         >
-//           <MenuItem value="">
-//             <em>None</em>
-//           </MenuItem>
-//           {Object.entries(selectedStateId).map(([k,v]) => (
-//         <MenuItem key ={k} value={v.stateId}></MenuItem>
-//       ))}
-//         </Select>
-//         <FormHelperText>Required</FormHelperText>
-//       </FormControl>
-
-//     </div>
-//   );
-// }
 
 export let selectedStateId = '';
 console.log(selectedStateId)
