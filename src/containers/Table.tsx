985b4bf8e90bcf3ee7528dd036fbf92a58a78070
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-08-16 13:57:50 -0700

    remove unused component

--- src/containers/Table.tsx
+++ src/containers/Table.tsx
@@ -13,7 +13,6 @@ import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import {PureComponent} from 'react'
 import { PieChart, Pie, Legend, Cell, ResponsiveContainer } from 'recharts';
 
 const authToken = process.env.REACT_APP_CENSUS_TOKEN;
